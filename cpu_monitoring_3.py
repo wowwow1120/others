0adeae4576010d35c043d74a54cbdb8546eb1472
wowwow1120 <50578092+wowwow1120@users.noreply.github.com>
2019-06-27 15:12:48 +0900

    Add files via upload

--- cpu_monitoring_3.py
+++ cpu_monitoring_3.py
@@ -6,6 +6,7 @@ import psutil
 import threading
 import subprocess
 import configparser
+import time
 
 
 def use_ini_file():
@@ -18,7 +19,7 @@ def use_ini_file():
 
     pid_num_list = []
     for processes in process_list:
-        grep_pid = "ps -ef | grep " + str(processes) + " | grep -v auto"
+        grep_pid = "ps -ef | grep " + str(processes) + " | grep -v auto | grep -v grep"
         grep_pid = subprocess.check_output(grep_pid, shell=True)
         grep_pid = grep_pid.decode()
         grep_pid = grep_pid.split('\n')
@@ -40,33 +41,30 @@ def display_progressbar(value, end_value, bar_length=40):
 
 
 def updateit():
+    #time.sleep(5)
     threading.Timer(int(sys.argv[1]), updateit).start()
     cpu_count = psutil.cpu_count()
-    cpu_stats = psutil.cpu_stats()
-    cpu_freq = psutil.cpu_freq()
-    cpu_load = psutil.getloadavg()
+    # cpu_stats = psutil.cpu_stats()
+    # cpu_freq = psutil.cpu_freq()
+    # cpu_load = psutil.getloadavg()
     cpu_usage = psutil.cpu_percent(interval=1, percpu=True)
     ram_usage = psutil.virtual_memory().percent
 
     pid_num_list = use_ini_file()
-    print(pid_num_list)
-    
-    # cpu_num = 0
 
     print('\n', "CPU_count = ", cpu_count)
-    print('\n', "[CPU_status]", '\n', "ctx_switches = ",cpu_stats[0],'\n',
-          "interrupts = ",cpu_stats[1],'\n',"soft_interrupts = ",cpu_stats[2],'\n', "syscalls = ",cpu_stats[3])
-    print('\n', "[CPU_freq]", '\n', "current = ",cpu_freq[0], '\n', "min = ",cpu_freq[1],'\n', "max = ",cpu_freq[2])
-    print('\n', "[CPU_load]", '\n', "load = ",cpu_load)
+    # print('\n', "[CPU_status]", '\n', "ctx_switches = ",cpu_stats[0],'\n',
+    #       "interrupts = ",cpu_stats[1],'\n',"soft_interrupts = ",cpu_stats[2],'\n', "syscalls = ",cpu_stats[3])
+    # print('\n', "[CPU_freq]", '\n', "current = ",cpu_freq[0], '\n', "min = ",cpu_freq[1],'\n', "max = ",cpu_freq[2])
+    # print('\n', "[CPU_load]", '\n', "load = ",cpu_load)
 
     for i in range(cpu_count):
         pid_info_list = []
-
         for j in pid_num_list:
             check_command_pid = "ps -o pid -p " + str(j)
             check_command_psr = "ps -o psr -p " + str(j)
             check_command_comm = "ps -o comm -p " + str(j)
-            check_command_pcpu = "ps -o pcpu -p " + str(j)
+            # check_command_pcpu = "ps -o pcpu -p " + str(j)
 
             pid_num = subprocess.check_output(check_command_pid, shell=True)
             pid_num = pid_num.decode()
@@ -83,24 +81,25 @@ def updateit():
             pid_process_name = pid_process_name.split('\n')[1]
             pid_process_name = pid_process_name.split(' ')[-1]
 
-            pid_usage = subprocess.check_output(check_command_pcpu, shell=True)
-            pid_usage = pid_usage.decode()
-            pid_usage = pid_usage.split('\n')[1]
-            pid_usage = pid_usage.split(' ')[-1]
+            # pid_usage = subprocess.check_output(check_command_pcpu, shell=True)
+            # pid_usage = pid_usage.decode()
+            # pid_usage = pid_usage.split('\n')[1]
+            # pid_usage = pid_usage.split(' ')[-1]
 
-            if pid_cpu == i:
-                pid_info_list.append(pid_num, pid_process_name, pid_usage)
+            if str(pid_cpu) == str(i):
+                x = [pid_num, pid_process_name]
+                pid_info_list.append(x)
 
         per_cpu_usage = cpu_usage[i]
 
         print("CPU", i, " : ", pid_info_list, per_cpu_usage)
         # print("CPU", cpu_num, " : ", pid_list)
 
-        cpu_usage_bar_value = str(per_cpu_usage).split(',')[0]
-        cpu_usage_bar_value = cpu_usage_bar_value.split('(')[1]
-        cpu_usage_bar_value = cpu_usage_bar_value.split('=')[1]
-        # print('\n', "CPU", cpu_num, " : ", i)
-        print(display_progressbar(cpu_usage_bar_value, 100))
+        # cpu_usage_bar_value = str(per_cpu_usage).split(',')[0]
+        # cpu_usage_bar_value = cpu_usage_bar_value.split('(')[1]
+        # cpu_usage_bar_value = cpu_usage_bar_value.split('=')[1]
+        # # print('\n', "CPU", cpu_num, " : ", i)
+        # print(display_progressbar(cpu_usage_bar_value, 100))
         # cpu_num += 1
 
     print('\n', "--------------")
